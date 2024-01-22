46ac9d81e000b2fd167da28ca3223d516d46df95
shahon922 <shahon922@naver.com>
2024-01-22 22:17:31 +0900

    이동 수정

--- fixTown/Assets/Scripts/Controllers/fixTownCharacterController.cs
+++ fixTown/Assets/Scripts/Controllers/fixTownCharacterController.cs
@@ -6,10 +6,10 @@ using UnityEngine;
 public class fixTownCharacterController : MonoBehaviour
 {
 
-    //public delegate void OnMoveDel(Vector2 direction);
-    //public OnMoveDel OnMoveEvent;
+    public delegate void OnMoveDel(Vector2 direction);
+    public OnMoveDel OnMoveEvent;
 
-    public event Action<Vector2> OnMoveEvent;
+    //public event Action<Vector2> OnMoveEvent;
     
     public void CallMoveEvent(Vector2 direction)
     {
